158f2a5b8ba65eabfe83cf833cb8db1e30e8e868
Ella <ellamcglynn3@gmail.com>
2025-02-18 19:09:58 +0000

    initial commit

---
+++ src/test/java/com/tus/anyDo/Individual_Project/IndividualProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tus.anyDo.Individual_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndividualProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
